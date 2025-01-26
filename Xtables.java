53dbfbb2cd85ba2cd944815289900cad414b4fe7
sarayu10062005 <srijasarayu@gmail.com>
2025-01-26 03:17:47 -0800

    Add files via upload

---
+++ Xtables.java
@@ -0,0 +1,10 @@
+public class Xtables {
+    public static void main(String[] args) {
+        int n = 2;
+        int len = 10;
+        for (int i = 1; i <= len; i++) {
+            int result = n * i;
+            System.out.printf("%d  x %d  =  %d\n", n, i, result);
+        }
+    }
+}
